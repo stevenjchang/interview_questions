995537dca465373d8712a23ada59fd9babc90076
kattak <katrina.devaney@gmail.com>
2017-02-04 13:36:57 -0800

    Add question text 05

--- interview_cake/05_makingChange.js
+++ interview_cake/05_makingChange.js
@@ -1,18 +1,18 @@
-Imagine you landed a new job as a cashier...
-Your quirky boss found out that you're a programmer and has a weird request about something they've been wondering for a long time.
-
-Write a function that, given:
-
-an amount of money
-a list of coin denominations
-computes the number of ways to make amount of money with coins of the available denominations.
-
-Example: for amount=44 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your program would output 4—the number of ways to make 4¢ with those denominations:
-
-1¢, 1¢, 1¢, 1¢
-1¢, 1¢, 2¢
-1¢, 3¢
-2¢, 2¢
+// Imagine you landed a new job as a cashier...
+// Your quirky boss found out that you're a programmer and has a weird request about something they've been wondering for a long time.
+//
+// Write a function that, given:
+//
+// an amount of money
+// a list of coin denominations
+// computes the number of ways to make amount of money with coins of the available denominations.
+//
+// Example: for amount=44 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your program would output 4—the number of ways to make 4¢ with those denominations:
+//
+// 1¢, 1¢, 1¢, 1¢
+// 1¢, 1¢, 2¢
+// 1¢, 3¢
+// 2¢, 2¢
 
 Input = 27
 denominations = 1, 5, 10, 25
